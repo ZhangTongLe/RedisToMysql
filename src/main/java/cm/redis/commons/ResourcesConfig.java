ecd0545a1f0e5921567b002f7870761e27fd6e30
chinamobile <yanxuexu007@126.com>
2016-09-10 12:26:08 +0800

    提交测试

---
+++ src/main/java/cm/redis/commons/ResourcesConfig.java
@@ -0,0 +1,34 @@
+package cm.redis.commons;
+
+/**
+ * 接口配置类，用于记录portal接口、短信接口的注册信息
+ * @author chenxiaobing
+ *
+ */
+public class ResourcesConfig {
+	// 系统标识、系统名称
+	public final static String SYSTEM_ID="storm";     
+	public final static String SYSTEM_NAME="汕头移动业支中心实时计算系统";    
+	public final static String SYSTEM_COPYRIGHT="中国移动通信集团广东有限公司汕头分公司业务支持中心运营管理室";
+	
+	// redis集群数据库的数据库连接池配置信息，入口ip和端口
+	public final static int MAX_ACTIVE = 500;		 								//可用连接实例的最大数目，默认值为8；如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
+	public final static int MAX_IDLE = 8;			 								//控制一个pool最多有多少个状态为idle(空闲的)的jedis实例，默认值是8。
+	public final static int MAX_WAIT =100*1000;  								//等待可用连接的最大时间，单位毫秒，默认值为-1，表示永不超时。如果超过等待时间，则直接抛出JedisConnectionException；
+	public final static boolean TEST_ON_BORROW = true;					//在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
+	public final static int CLUSTER_TIMEOUT=15*1000;						//获取集群信息的超时时间
+	public final static int CLUSTER_MAX_REDIRECTIONS=5; 			//获取重定向的次数
+	
+	public final static String REDIS_SERVER_IP="10.245.254.56";		//redis数据库IP地址
+	public final static String REDIS_SERVER_PORT="7000";				//redis数据库端口
+	
+	// 数据枢纽接口的文件，以及数据存放路径，最后带有/
+	public final static String SYN_SERVER_DATAFILE="/home/storm/netantdata/ref/tomysql/";  //正式服务器地址
+	//public final static String SYN_SERVER_DATAFILE="E:/";  					   //测试地址
+	
+	public final static String MYSQL_SERVER_DRIVER="com.mysql.jdbc.Driver";	             		//mysql数据库驱动
+	public final static String MYSQL_SERVER_URL="jdbc:mysql://10.245.254.55:3306/dtdb";	//数据库端口
+	public final static String MYSQL_USER="mysqlc";																//mysql用户名stdt
+	public final static String MYSQL_PASSWD="mysqlc";															//mysql密码ST.dt.0505.903
+
+}
---
+++ src/main/java/cm/redis/settomysql/Redis_To_Mysql.java
@@ -0,0 +1,154 @@
+package cm.redis.settomysql;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+import org.apache.log4j.Logger;
+
+import cm.redis.commons.RedisServer;
+import cm.redis.commons.ResourcesConfig;
+import cm.redis.commons.TimeFormatter;
+
+
+/**
+ * 定时将当天最新的redis数据推送到mysql中，实现数据的持久化
+ * @author nicolashsu 2016-09-08
+ *
+ */
+public class Redis_To_Mysql {
+
+	public static Logger logger=Logger.getLogger(Redis_To_Mysql.class);
+	
+	/**
+	 * 主函数
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		//将hotspot，业务关注的热点区域的人流量，流量数据信息，推送到mysql中
+		//表格是tb_mofang_hotspot_flow_today，字段data_time，id，day，hour，minute，people_cnt，net_flow
+		RedisServer redisserver=null;
+		TreeSet<String> keys=null; 
+		Iterator<String> keylist =null;
+		String[] keysplit=null;
+		String cdate=null;
+		int num=0;
+		
+		String key=null;
+		
+		String data_time=null;
+		String id=null;
+		String day=null;
+		String hour=null;
+		String minute=null;
+		long pcnt=0;
+		String people_cnt=null;
+		double dnflow=0.0;
+		long lnflow=0;
+		String net_flow=null;
+		String filepath=null;
+		FileWriter fw=null;	
+		
+		Connection conn=null;
+		String sql=null;
+		String url=ResourcesConfig.MYSQL_SERVER_URL+"?user="+ ResourcesConfig.MYSQL_USER
+				+"&password="+ResourcesConfig.MYSQL_PASSWD+"&characterEncoding=UTF8";
+		Statement stmt=null;
+		boolean sqlok=false;
+		try {
+			while(true)
+			{
+				logger.info(" Start to get hotspot redis-keys");
+				//获取实例
+				redisserver=RedisServer.getInstance();		
+				data_time=TimeFormatter.getNow(); 	//获取当前时间YYYY-MM-DD HH:mm:ss
+				day=TimeFormatter.getDate(); 			//当天日期 YYYYMMDD，删除mysql对应的当天记录		
+				cdate=data_time.substring(0,10);        //获取当前日期YYYY-MM-DD
+				keys=redisserver.keys("mfg4_"+cdate+"_hspset_*"); 				//获取所有的hotspot对应的imsi集合
+				num=0;
+				
+				conn=null;
+				
+				filepath=ResourcesConfig.SYN_SERVER_DATAFILE+"tb_mofang_hotspot_flow_today.txt";
+				File file = new File(filepath);
+				if (!file.isDirectory()) { 
+					fw=new FileWriter(file);
+					fw.write("");
+					keylist = keys.iterator();
+					while(keylist.hasNext())
+					{
+						key=keylist.next().toString();
+						keysplit=key.split("_");
+						if(keysplit.length==6)
+						{
+							id=keysplit[5];  							//hotspotid
+							hour=keysplit[3]; 						//hour
+							minute=keysplit[4]; 					//minute
+							pcnt=redisserver.scard(key);
+							if(pcnt<0)pcnt=0;
+							people_cnt=String.valueOf(pcnt); //people_cnt
+							key="mfg4_"+cdate+"_hspflux_"+hour+"_"+minute+"_"+id;
+							net_flow=redisserver.get(key);
+							if(net_flow!=null)dnflow=Double.valueOf(net_flow);
+							lnflow=(long)dnflow;
+							net_flow=String.valueOf(lnflow);  //net_flow
+							key="'"+data_time+"','"+id+"','"+day+"','"+hour+"','"+minute+"','"+people_cnt+"','"+lnflow+"'\n";
+							fw.write(key);
+							num=num+1;
+						}
+					}
+					fw.close();
+			    }	
+				logger.info(" Complete get hotspot redis-keys, get "+num+" records");	
+				
+				try{
+					Class.forName(ResourcesConfig.MYSQL_SERVER_DRIVER);
+					conn=DriverManager.getConnection(url);
+					stmt =conn.createStatement();
+					sql="load data local infile '"+filepath+"' replace into table tb_mofang_hotspot_flow_today fields terminated by ',' enclosed by '\\'' lines terminated by '\\n'";
+					sqlok=stmt.execute(sql);
+					logger.info(" Set into mysql ok");
+				}catch(Exception e){
+					logger.info(" Thread Redis_To_Mysql crashes: "+e.getMessage());
+				}finally {
+					conn.close();
+				}
+				
+				//释放内存
+				redisserver=null;
+				keys=null;
+				keylist=null;
+				keysplit=null;
+				key=null;
+				cdate=null;
+				data_time=null;
+				id=null;
+				day=null;
+				hour=null;
+				minute=null;
+				pcnt=0;
+				people_cnt=null;
+				dnflow=0.0;
+				lnflow=0;
+				num=0;
+				net_flow=null;
+				filepath=null;
+				fw=null;
+				
+				conn=null;
+				sql=null;
+				
+				Thread.sleep(1000*60*5);
+			}
+		} catch (Exception e) {
+			logger.info(" Thread Flush_Redis_DB crashes: "+e.getMessage());
+		}
+		
+		
+	}
+}
