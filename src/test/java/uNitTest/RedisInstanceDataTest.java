1b3a4578e3d52b415af4565a49c53e4740211a10
chinamobile <yanxuexu007@126.com>
2017-05-09 16:49:09 +0800

    666

--- src/test/java/uNitTest/RedisInstanceDataTest.java
+++ src/test/java/uNitTest/RedisInstanceDataTest.java
@@ -129,8 +129,8 @@ public class RedisInstanceDataTest {
 //						decch=decch.replace("旗舰店", "");
 //						decch=decch.replace("官方", "");
 						if(StringUtils.contains(decch, "游戏")==true||StringUtils.contains(decch, "视频")==true||StringUtils.contains(decch, "音频")==true){
-						key="mfg4_"+tdate+"_AppUse_"+chineselist.get(i);//,_ebusiw_,_Zh_,_baiduw_,_AppUse_,_IntidUse_
-						System.out.println((i+1)+":"+decch+":"+redisServer.get(key));//
+							key="mfg4_"+tdate+"_AppUse_"+chineselist.get(i);//,_ebusiw_,_Zh_,_baiduw_,_AppUse_,_IntidUse_
+							System.out.println((i+1)+":"+decch+":"+redisServer.get(key));//
 						}
 					}
 				}
