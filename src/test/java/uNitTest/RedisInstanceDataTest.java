c61c6108d02743049a02dea1040d683e27929773
chinamobile <chinamobile@stxuxueyan1.st.gmcc.net>
2017-06-22 12:12:01 +0800

    666

--- src/test/java/uNitTest/RedisInstanceDataTest.java
+++ src/test/java/uNitTest/RedisInstanceDataTest.java
@@ -3,11 +3,26 @@ package uNitTest;
 import java.util.List;
 import java.util.TreeSet;
 
-import org.apache.commons.codec.binary.Base64;
-
 import cm.redis.commons.RedisServer;
 import cm.redis.commons.TimeFormatter;
 import redis.clients.jedis.SortingParams;
+//import java.text.SimpleDateFormat;  
+//import java.util.Date;  
+//import org.apache.log4j.Logger;
+//
+//import java.io.BufferedReader;  
+//import java.io.IOException;  
+//import java.io.InputStream;  
+//import java.io.InputStreamReader;  
+//import java.io.OutputStreamWriter;
+//import java.net.Proxy;
+//import java.net.InetSocketAddress;
+//import java.net.URL;  
+//import java.net.URLConnection;  
+//
+//import org.apache.xerces.impl.dv.util.Base64;  
+  
+//import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;  
 
 public class RedisInstanceDataTest {
 	public static void main(String[] args) {
@@ -224,4 +239,69 @@ public class RedisInstanceDataTest {
 		
 		RedisServer.close();
 	}
+	 
+//	public static double[] postBaidu(double lng, double lat) {  
+//        double[] latlng = null;  
+//  
+//        URL url = null;  
+//        URLConnection connection = null;
+//        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("cmproxy.gmcc.net", 8081));
+//        try {  
+//            url = new URL("http://api.map.baidu.com/ag/coord/convert?from=0&to=4&x=" + String.valueOf(lng) + "&y="+String.valueOf(lat));  
+//            connection = url.openConnection(proxy);  
+//            connection.setConnectTimeout(5000);  
+//            connection.setReadTimeout(5000);  
+//            connection.setDoOutput(true);  
+//            OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream(), "utf-8");  
+//            out.flush();  
+//            out.close();  
+//  
+//            // 服务器的回应的字串，并解析  
+//            String sCurrentLine;  
+//            String sTotalString;  
+//            sCurrentLine = "";  
+//            sTotalString = "";  
+//            InputStream l_urlStream;  
+//            l_urlStream = connection.getInputStream();  
+//            BufferedReader l_reader = new BufferedReader(new InputStreamReader(l_urlStream));  
+//            while ((sCurrentLine = l_reader.readLine()) != null) {  
+//                if (!sCurrentLine.equals(""))  
+//                    sTotalString += sCurrentLine;  
+//            }  
+//            // System.out.println(sTotalString);  
+//            sTotalString = sTotalString.substring(1, sTotalString.length() - 1);  
+//            // System.out.println(sTotalString);  
+//            String[] results = sTotalString.split("\\,");  
+//            if (results.length == 3) {  
+//                if (results[0].split("\\:")[1].equals("0")) {  
+//                    String mapX = results[1].split("\\:")[1];  
+//                    String mapY = results[2].split("\\:")[1];  
+//                    mapX = mapX.substring(1, mapX.length() - 1);  
+//                    mapY = mapY.substring(1, mapY.length() - 1);  
+//                    mapX = new String(Base64.decode(mapX));  
+//                    mapY = new String(Base64.decode(mapY));  
+//                    // System.out.println(mapX);  
+//                    // System.out.println(mapY);  
+//                    latlng = new double[] { Double.parseDouble(mapX), Double.parseDouble(mapY) };  
+//                } else {  
+//                    System.out.println("error != 0");  
+//                }  
+//            } else {  
+//                System.out.println("String invalid!");  
+//            }  
+//        } catch (Exception e) {  
+//            e.printStackTrace();  
+//            System.out.println("GPS转百度坐标异常！");  
+//        }  
+//        //SimpleDateFormat dateFormat1 = new SimpleDateFormat("HH:mm:ss");  
+//        //log.info("百度GPS===" + dateFormat1.format(new Date()) + " " + latlng[0] + " " + latlng[1]);  
+//        return latlng;  
+//    }  
+//  
+//    public static void main(String[] args) throws IOException {  
+//        double lng =116.718611; //114.42285333333334;  
+//        double lat =23.369444; //30.459873333333334;  
+//        double[] latlng = RedisInstanceDataTest.postBaidu(lng, lat);  
+//        System.out.println("lng===" + latlng[0] + "  lat===" + latlng[1]);  
+//    }  
 }
